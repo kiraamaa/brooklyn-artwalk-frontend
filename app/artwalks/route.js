b0ed7e303548674d0bca19e419a67ae765274774
kiraamaa <kira.a.maclean@gmail.com>
2017-01-15 21:08:41 -0500

    Add favorite model
    Began CRUD functionality but having trouble rendering content.
    Return empty object for favorite.

--- app/artwalks/route.js
+++ app/artwalks/route.js
@@ -4,36 +4,13 @@ export default Ember.Route.extend({
   model () {
     return this.get('store').findAll('artwalk');
   },
+  actions: {
+    editList (list) {
+      console.log("inside lists route, list is ", list);
+      this.transitionTo('list/edit', list);
+    },
+    deleteList (list) {
+      list.destroyRecord();
+    },
+  },
 });
-
-// export default Ember.Route.extend({
-//   model () {
-//     return this.get('store').findAll('artwalk');
-//   },
-//   actions: {
-//     editArtwalk (artwalk) {
-//       console.log("inside artwalks route, artwalk is ", artwalk);
-//       this.transitionTo('artwalk/edit', artwalk);
-//     },
-//     deleteArtwalk (artwalk) {
-//       artwalk.destroyRecord();
-//     },
-//   },
-// });
-
-// let artwalks = [{
-//   // id: 1,
-//   title: 'Cool walk'
-// }, {
-//   // id: 2,
-//   title: 'Other walk'
-// }, {
-//   // id: 3,
-//   title: 'Downtown walk'
-// }];
-//
-// export default Ember.Route.extend({
-//   model() {
-//     return artwalks;
-//   },
-// });
--- app/favorites/route.js
+++ app/favorites/route.js
@@ -1,9 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  // model () {
-  //   return this.get('store').findAll('favorites');
-  // },
+  model (favorite) {
+    console.log(favorite.artwalkId);
+    return this.get('store').findAll('favorite');
+  },
   // actions: {
   //   editFavorite (favorite) {
   //     console.log("inside favorites route, favorite is ", favorite);
